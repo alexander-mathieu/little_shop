0184ee3476b53fd20464520c9686285a0e980ff4
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-03 16:58:55 -0600

    Add Profile Addresses spec with initial tests

---
+++ spec/features/users/addresses/profile_addresses_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe 'Profile Addresses page', type: :feature do
+  before :each do
+    @user_1 = create(:user)
+    @user_2 = create(:user)
+  end
+
+  context 'as a user with no addresses' do
+    it 'should show a message when user has no addresses' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
+
+      visit profile_addresses_path
+
+      expect(page).to have_content("You don't have any addresses yet.")
+    end
+  end
+
+  context 'as a user with multiple addresses' do
+    before :each do
+      @address_1 = @user_1.addresses.create!(zip: "Zip 1", address: "Address 1", state: "State 1", city: "City 1")
+      @address_2 = @user_1.addresses.create!(zip: "Zip 2", address: "Address 2", state: "State 2", city: "City 2", nickname: "Nickname 2")
+      @address_3 = @user_1.addresses.create!(zip: "Zip 3", address: "Address 3", state: "State 3", city: "City 3", nickname: "Nickname 3")
+      @address_4 = @user_1.addresses.create!(zip: "Zip 4", address: "Address 4", state: "State 4", city: "City 4", nickname: "Nickname 4")
+    end
+
+    it 'should display all addresses for the user' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+      visit profile_addresses_path
+
+      within "#address-#{@address_1.id}" do
+        expect(page).to have_content(@address_1.nickname)
+        expect(page).to have_content("#{@address_1.address}, #{@address.city}, #{@address.state} #{@address.zip}")
+      end
+
+      within "#address-#{@address_1.id}" do
+        expect(page).to have_content(@address_1.nickname)
+        expect(page).to have_content("#{@address_1.address}, #{@address.city}, #{@address.state} #{@address.zip}")
+      end
+
+      within "#address-#{@address_1.id}" do
+        expect(page).to have_content(@address_1.nickname)
+        expect(page).to have_content("#{@address_1.address}, #{@address.city}, #{@address.state} #{@address.zip}")
+      end
+
+      within "#address-#{@address_1.id}" do
+        expect(page).to have_content(@address_1.nickname)
+        expect(page).to have_content("#{@address_1.address}, #{@address.city}, #{@address.state} #{@address.zip}")
+      end
+    end
+  end
+end
