868b674145c46afb7bcbd8cf93605c5eb3d0997a
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-03 18:33:29 -0600

    Add create action and strong params to Profile AddressesController

--- app/controllers/profile/addresses_controller.rb
+++ app/controllers/profile/addresses_controller.rb
@@ -7,6 +7,25 @@ class Profile::AddressesController < ApplicationController
   end
 
   def new
+    @address = Address.new
+  end
 
+  def create
+    user = current_user
+    address = user.addresses.new(address_params)
+
+    if address.save
+      flash[:success] = "Address added!"
+      redirect_to profile_addresses_path
+    else
+      flash.now[:danger] = address.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:address, :city, :state, :zip, :nickname, :user_id)
   end
 end
