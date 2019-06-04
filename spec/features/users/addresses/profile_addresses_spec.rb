f3a28b0180e027d2a4486e55e39494c53dcd0f0b
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-03 21:55:23 -0600

    Add feature specs for User editing and deleting Addresses from Address index

--- spec/features/users/addresses/profile_addresses_spec.rb
+++ spec/features/users/addresses/profile_addresses_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe 'Profile Addresses page', type: :feature do
       @address_4 = @user_1.addresses.create!(zip: "Zip 4", address: "Address 4", state: "State 4", city: "City 4", nickname: "Nickname 4")
     end
 
-    it 'should display all addresses for the user' do
+    it 'the page should display all addresses for the user' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
 
       visit profile_addresses_path
@@ -80,6 +80,147 @@ RSpec.describe 'Profile Addresses page', type: :feature do
       within "#address-#{@address_4.id}" do
         expect(page).to have_content(@address_4.nickname)
         expect(page).to have_content("#{@address_4.address} #{@address_4.city}, #{@address_4.state} #{@address_4.zip}")
+      end
+    end
+
+      it 'the page should display a link to edit each address' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+        visit profile_addresses_path
+
+        within "#address-#{@address_1.id}" do
+          expect(page).to have_link('Edit')
+        end
+
+        within "#address-#{@address_2.id}" do
+          expect(page).to have_link('Edit')
+        end
+
+        within "#address-#{@address_3.id}" do
+          expect(page).to have_link('Edit')
+        end
+
+        within "#address-#{@address_4.id}" do
+          expect(page).to have_link('Edit')
+        end
+      end
+
+      it 'the page should display a link to delete each address' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+        visit profile_addresses_path
+
+        within "#address-#{@address_1.id}" do
+          expect(page).to have_link('Delete')
+        end
+
+        within "#address-#{@address_2.id}" do
+          expect(page).to have_link('Delete')
+        end
+
+        within "#address-#{@address_3.id}" do
+          expect(page).to have_link('Delete')
+        end
+
+        within "#address-#{@address_4.id}" do
+          expect(page).to have_link('Delete')
+        end
+      end
+
+      describe "and click 'Edit' beside an address name" do
+        it "I'm brought to an edit form for that address" do
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+          visit profile_addresses_path
+
+          within "#address-#{@address_1.id}" do
+            click_link 'Edit'
+          end
+
+          expect(current_path).to eq(edit_profile_address_path(@address_1.id))
+        end
+
+        describe "and fill out all the form correctly" do
+          before :each do
+            @updated_zip = 'updated zip'
+            @updated_city = 'updated city'
+            @updated_state = 'updated state'
+            @updated_address = 'updated address'
+            @updated_nickname = 'updated nickname'
+          end
+
+          it "I see a notifcation and the updated information on my address list" do
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+            visit profile_addresses_path
+
+            within "#address-#{@address_1.id}" do
+              click_link 'Edit'
+            end
+
+            fill_in :address_zip, with: @updated_zip
+            fill_in :address_city, with: @updated_city
+            fill_in :address_state, with: @updated_state
+            fill_in :address_address, with: @updated_address
+            fill_in :address_nickname, with: @updated_nickname
+
+            click_button 'Save Address'
+
+            expect(current_path).to eq (profile_addresses_path)
+
+            expect(page).to have_content("Address updated.")
+
+            within "#address-#{@address_1.id}" do
+              expect(page).to have_content(@updated_nickname)
+              expect(page).to have_content("#{@updated_address} #{@updated_city}, #{@updated_state} #{@updated_zip}")
+            end
+          end
+        end
+
+        describe "and fill out the edit form partially" do
+          before :each do
+            @updated_zip = 'updated zip'
+            @updated_city = 'updated city'
+            @updated_state = 'updated state'
+            @updated_address = 'updated address'
+            @updated_nickname = 'updated nickname'
+          end
+
+          it "I see a notifcation and the updated information on my address list" do
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+            visit profile_addresses_path
+
+            within "#address-#{@address_1.id}" do
+              click_link 'Edit'
+            end
+
+            fill_in :address_zip, with: @updated_zip
+            fill_in :address_address, with: @updated_address
+            fill_in :address_nickname, with: @updated_nickname
+
+            click_button 'Save Address'
+
+            expect(page).to have_content(@updated_nickname)
+            expect(page).to have_content("#{@updated_address} #{@address_1.city}, #{@address_1.state} #{@updated_zip}")
+          end
+        end
+      end
+
+      describe "and click 'Delete' beside an address name" do
+        it "I no longer see that address on my address list" do
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+          visit profile_addresses_path
+
+          within "#address-#{@address_1.id}" do
+            click_link 'Delete'
+          end
+
+          expect(page).to have_content("Address deleted.")
+          expect(page).to_not have_css("#address-#{@address_1.id}")
+
+          expect(@user_1.addresses.count).to eq(3)
       end
     end
   end
