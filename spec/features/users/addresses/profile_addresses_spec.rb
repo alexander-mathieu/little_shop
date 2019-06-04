832ac0ccbcaf0a7b99c7dba2844dc097bcbd4b24
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-04 14:12:14 -0600

    Add feature spec for User seeing edit/delete buttons on only addresses that have not been used in completed orders

--- spec/features/users/addresses/profile_addresses_spec.rb
+++ spec/features/users/addresses/profile_addresses_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'Profile Addresses page', type: :feature do
   before :each do
     @user_1 = create(:user)
     @user_2 = create(:user)
+
+    @merchant = create(:merchant)
   end
 
   context 'as a registered user with no addresses' do
@@ -55,6 +57,25 @@ RSpec.describe 'Profile Addresses page', type: :feature do
       @address_2 = @user_1.addresses.create!(zip: "Zip 2", address: "Address 2", state: "State 2", city: "City 2", nickname: "Nickname 2")
       @address_3 = @user_1.addresses.create!(zip: "Zip 3", address: "Address 3", state: "State 3", city: "City 3", nickname: "Nickname 3")
       @address_4 = @user_1.addresses.create!(zip: "Zip 4", address: "Address 4", state: "State 4", city: "City 4", nickname: "Nickname 4")
+
+      @i1, @i2, @i3, @i4 = create_list(:item, 4, user: @merchant)
+
+      @o1 = create(:order, address: @address_1)
+      @o2 = create(:order, address: @address_2)
+      @o5 = create(:order, address: @address_3)
+      @o6 = create(:order, address: @address_4)
+      @o3 = create(:shipped_order, address: @address_1)
+      @o4 = create(:cancelled_order, address: @address_2)
+
+      create(:order_item, order: @o1, item: @i1, quantity: 1, price: 2)
+      create(:order_item, order: @o1, item: @i2, quantity: 2, price: 2)
+      create(:order_item, order: @o2, item: @i2, quantity: 4, price: 2)
+      create(:order_item, order: @o3, item: @i1, quantity: 4, price: 2)
+      create(:order_item, order: @o4, item: @i2, quantity: 5, price: 2)
+      create(:order_item, order: @o5, item: @i3, quantity: 8, price: 2)
+      create(:order_item, order: @o5, item: @i4, quantity: 10, price: 2)
+      create(:order_item, order: @o6, item: @i3, quantity: 8, price: 2)
+      create(:order_item, order: @o6, item: @i4, quantity: 12, price: 2)
     end
 
     it 'the page should display all addresses for the user' do
@@ -83,13 +104,13 @@ RSpec.describe 'Profile Addresses page', type: :feature do
       end
     end
 
-      it 'the page should display a link to edit each address' do
+      it 'the page should display a link to edit addresses that have not been used in completed orders' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
 
         visit profile_addresses_path
 
         within "#address-#{@address_1.id}" do
-          expect(page).to have_link('Edit')
+          expect(page).to_not have_link('Edit')
         end
 
         within "#address-#{@address_2.id}" do
@@ -105,13 +126,13 @@ RSpec.describe 'Profile Addresses page', type: :feature do
         end
       end
 
-      it 'the page should display a link to delete each address' do
+      it 'the page should display a link to delete addresses that have not been used in completed orders' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
 
         visit profile_addresses_path
 
         within "#address-#{@address_1.id}" do
-          expect(page).to have_link('Delete')
+          expect(page).to_not have_link('Delete')
         end
 
         within "#address-#{@address_2.id}" do
@@ -133,11 +154,11 @@ RSpec.describe 'Profile Addresses page', type: :feature do
 
           visit profile_addresses_path
 
-          within "#address-#{@address_1.id}" do
+          within "#address-#{@address_2.id}" do
             click_link 'Edit'
           end
 
-          expect(current_path).to eq(edit_profile_address_path(@address_1.id))
+          expect(current_path).to eq(edit_profile_address_path(@address_2.id))
         end
 
         describe "and fill out all the form correctly" do
@@ -154,7 +175,7 @@ RSpec.describe 'Profile Addresses page', type: :feature do
 
             visit profile_addresses_path
 
-            within "#address-#{@address_1.id}" do
+            within "#address-#{@address_2.id}" do
               click_link 'Edit'
             end
 
@@ -170,7 +191,7 @@ RSpec.describe 'Profile Addresses page', type: :feature do
 
             expect(page).to have_content("Address updated.")
 
-            within "#address-#{@address_1.id}" do
+            within "#address-#{@address_2.id}" do
               expect(page).to have_content(@updated_nickname)
               expect(page).to have_content("#{@updated_address} #{@updated_city}, #{@updated_state} #{@updated_zip}")
             end
@@ -191,7 +212,7 @@ RSpec.describe 'Profile Addresses page', type: :feature do
 
             visit profile_addresses_path
 
-            within "#address-#{@address_1.id}" do
+            within "#address-#{@address_2.id}" do
               click_link 'Edit'
             end
 
@@ -202,7 +223,7 @@ RSpec.describe 'Profile Addresses page', type: :feature do
             click_button 'Save Address'
 
             expect(page).to have_content(@updated_nickname)
-            expect(page).to have_content("#{@updated_address} #{@address_1.city}, #{@address_1.state} #{@updated_zip}")
+            expect(page).to have_content("#{@updated_address} #{@address_2.city}, #{@address_2.state} #{@updated_zip}")
           end
         end
       end
@@ -213,12 +234,12 @@ RSpec.describe 'Profile Addresses page', type: :feature do
 
           visit profile_addresses_path
 
-          within "#address-#{@address_1.id}" do
+          within "#address-#{@address_2.id}" do
             click_link 'Delete'
           end
 
           expect(page).to have_content("Address deleted.")
-          expect(page).to_not have_css("#address-#{@address_1.id}")
+          expect(page).to_not have_css("#address-#{@address_2.id}")
 
           expect(@user_1.addresses.count).to eq(3)
       end
